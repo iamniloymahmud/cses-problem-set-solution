0dd639900bda1c9e5383620c90cf427c5f8dbe8e
Jaied Bin Mahmud <73630418+iamniloymahmud@users.noreply.github.com>
2024-09-11 21:35:56 +0600

    Update Repetitions.cpp

--- Repetitions.cpp
+++ Repetitions.cpp
@@ -3,9 +3,9 @@
 #include <cstdlib>
 #include <cctype>
 #include <cmath>
- 
-//NILOY MAHMUD, KUET BME'18
- 
+
+//JAIED BIN MAHMUD, KUET BME'18
+
 #include <algorithm>
 #include <set>
 #include <queue>
@@ -20,66 +20,84 @@
 #include <map>
 #include <iterator>
 #include <iomanip>
- 
- 
+#include <bitset>
+
+
 using namespace std;
- 
- 
+
+
 #define all(c) c.begin(),c.end()
-#define rall(c) c.rbegin(),c.rend()
-#define allb(c) c.begin(),c.end(),c.begin()
 #define gcd(a,b) __gcd(abs(a),abs(b))
-#define lcm(a,b) (((a)/(gcd(a,b)))*(b))
+#define lcm(a,b) (((a)/(__gcd(a,b)))*(b))
 #define isodd(a) ((a)&1)
 #define iseven(a) !((a)&1)
+#define mod(a,b) (a-(a/b)*b)
 #define sync() ios_base::sync_with_stdio(false),cin.tie(nullptr)
 #define pii pair<int,int>
 #define pll pair<long long, long long>
-#define mp map<string,int>
-#define mii map<int,int>
+#define rep(i,s,n) for(int i = s; i < n; ++i)
+#define per(i,s,e) for(int i = s; i >= e; --i)
 #define tr(it,c) for(auto & it : (c))
 #define rtr(it,c) for(auto it = (c).rbegin(); it != (c).rend(); it++)
 #define ll long long
 #define endl "\n"
 #define abs(x) (((x) < 0) ? -(x) : (x))
-#define swp(a,b) (a = a^b,b = a^b,a = a^b)
 #define clr(x,y) memset((x),(y),sizeof(x))
 #define popcount(x) __builtin_popcount(x)
-#define fileout() freopen("output.txt", "w", stdout)
-#define filein() freopen("input.txt", "r",stdin)
-#define error() freopen("error.txt","w",stderr)
+#define fileout(x) freopen(x, "w", stdout)
+#define filein(x) freopen(x, "r",stdin)
+#define error(x) freopen(x,"w",stderr)
 #define sqr(x) ((x)*(x))
 #define cube(x) ((x)*(x)*(x))
-#define inf 1<<30
-#define linf 1<<62
-#define mod 1000000007
+#define modulus 1000000007
+#define mod(a,b) ((a)%b+b)%b
 #define pi acos(-1.)
-#define valid(x,y,row,col) (((x) >= 0 and (x) < row) and ((y) >= 0 and (y) < col))
-#define debug(...) fprintf(stderr, __VA_ARGS__), fflush(stderr)
-#define timer(d) for(long blockTime=NULL;(blockTime==NULL?(blockTime=clock())!=NULL:false); debug("%s:%.4fs",d,(double)(clock()-blockTime)/CLOCKS_PER_SEC))
- 
-int x4[] = {0,0,1,-1};
-int y4[] = {1,-1,0,0};
- 
+#define ncout(var,i,n) cout << var << " \n" [i == n-1];
+#define gap " "
+#define gapn " \n"
+#define byte(num,bits) bitset<bits> (num)
+#define valid(x,y,startrow,startcol,row,col) (((x) >= startrow and (x) < row) and ((y) >= startcol and (y) < col))
+
+ostream & operator << (ostream & os, pair<int,int> a)
+{
+    return os << a.first << " " << a.second;
+}
+
+
+int x4[] = {0,1,0,-1};
+int y4[] = {1,0,-1,0};
+
+int knight_x[] = {-2,-1,1,2,2,1,-1,-2};
+int knight_y[] = {1, 2, 2,1,-1,-2,-2,-1};
+
+class CompareByParameter{
+public:
+    int para;
+    CompareByParameter(int m){
+        para = m;
+    }
+    bool operator () (int i){
+        return i < para;
+    }
+};
+
+
+
 int main()
 {
     sync();
-    int n;
     string x;
     cin >> x;
-    n = x.length();
-    int i = 0;
-    int counter = -inf;
-    while(i < n){
-        int y = 0;
-        char ch = x[i];
-        while(ch == x[i]){
-            i++;
-            y++;
-        }
-        counter = max(counter,y);
+    int n = x.size();
+    int answer = 1;
+    int temp = 1;
+    rep(i,1,n) {
+        if(x[i] == x[i-1])temp++;
+        else temp = 1;
+        answer = max(answer, temp);
     }
-    cout << counter << endl;
- 
+
+    cout << answer << endl;
+
     return 0;
 }
